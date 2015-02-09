acf81348309a5b6443e3d18481c099580694003c
Nicolas Berthier <nberth@member.fsf.org>
2015-02-09 16:29:38 +0100

    Add licence header

--- opam-dist.mk
+++ opam-dist.mk
@@ -1,8 +1,23 @@
 # -*- makefile-gmake -*-
+# ----------------------------------------------------------------------
+# 
+# Copyright (C) 2015 Nicolas Berthier
 #
-# Author: Nicolas Berthier
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 # ------------------------------------------------------------------------------
+#
 # Makefile utilities to build opam packages. To use it, define an environment
 # variable (e.g., OPAM_DEVEL_DIR) and make it refer the absolute directory
 # containting this `opam-dist.mk' file. Also add a `devel-config.mk' file in the
